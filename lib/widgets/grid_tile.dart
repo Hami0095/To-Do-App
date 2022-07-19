50a546db203386c5e7d66d3cf6537cdc75ec242e
Hami0095 <abdman0095@gmail.com>
2022-07-19 15:08:14 +0500

    UPDATE=> Grid Tile Done

--- lib/widgets/grid_tile.dart
+++ lib/widgets/grid_tile.dart
@@ -1,23 +1,85 @@
+import 'dart:core';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class MyGridTile extends StatelessWidget {
-  const MyGridTile({Key? key}) : super(key: key);
+  int index;
+  final String title;
+  final String tasks;
+  MyGridTile({
+    Key? key,
+    required this.index,
+    required this.title,
+    required this.tasks,
+  }) : super(key: key);
+
+  List mycolors = [
+    Colors.blueAccent,
+    Colors.amberAccent,
+    Colors.brown,
+    Colors.cyanAccent,
+    Colors.orangeAccent,
+    Colors.greenAccent,
+    Colors.indigoAccent,
+    Colors.yellowAccent,
+    Colors.limeAccent,
+    Colors.pinkAccent,
+    Colors.purpleAccent,
+    Colors.redAccent,
+    Colors.tealAccent
+  ];
+
+  Random _random = Random();
+
+  List icons = [
+    Icons.work,
+    Icons.person,
+    Icons.health_and_safety_rounded,
+    Icons.location_city,
+    Icons.add,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Column(
-        children: [
-          SizedBox.expand(
-            child: Row(
-              children: [
-                Text("tileTitle", style: Theme.of(context).textTheme.bodyLarge),
-                Text("Tasks rem",
-                    style: Theme.of(context).textTheme.labelSmall),
-              ],
+      child: Card(
+        elevation: 1.5,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                backgroundColor:
+                    Colors.primaries[_random.nextInt(Colors.primaries.length)]
+                        [_random.nextInt(13) * 100],
+                maxRadius: 20,
+                child: IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    icons[index <= icons.length ? index : 0],
+                    color: Colors
+                            .primaries[_random.nextInt(Colors.primaries.length)]
+                        [_random.nextInt(7) * 100],
+                    size: 18,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(title, style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(
+              height: 10,
+            ),
+            Text("$tasks tasks", style: Theme.of(context).textTheme.labelLarge),
+          ],
+        ),
       ),
     );
   }
