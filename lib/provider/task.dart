1823208159acf8df58515bc1e2ff3528b628bdc9
Hami0095 <abdman0095@gmail.com>
2022-07-20 10:39:31 +0500

    UPDATE=> Task_change_notifie-00

--- lib/provider/task.dart
+++ lib/provider/task.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Task {}
+class Task extends ChangeNotifier {
+  String? taskId;
+  String? title;
+  String? category;
+  bool state = false;
+  Task({
+    required this.taskId,
+    required this.title,
+    required this.category,
+    this.state = false,
+  });
+  void toggleState() {
+    state = state == false ? state : !state;
+  }
+}
